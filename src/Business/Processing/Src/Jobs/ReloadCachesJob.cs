f5263c76fdeb05611106a5e0fd9a32614716b2a0
Leonid Komarov <Leokom96@gmail.com>
2022-04-03 20:29:36 +0200

    Add reload caches job

---
+++ src/Business/Processing/Src/Jobs/ReloadCachesJob.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using NLog;
+using Objects.Dto;
+using Persistence.Src;
+using Quartz;
+
+namespace Processing.Src.Jobs
+{
+    public class ReloadCachesJob : IJob
+    {
+        // services
+        private readonly IDomainManager<TaskDto> _storage;
+
+        private readonly ILogger Logger = LogManager.GetLogger(nameof(ReloadCachesJob));
+
+        public ReloadCachesJob(IDomainManager<TaskDto> storage)
+        {
+            _storage = storage;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Logger.Info("job will reload caches...");
+
+            await _storage.ReloadStoreAsync();
+
+            Logger.Info("Caches has been reloaded");
+        }
+    }
+}
